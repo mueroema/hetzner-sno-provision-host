ee7ae87972af6639940408991f03970787cfb283
Mario M <muellma.gruena@googlemail.com>
2024-10-30 09:18:45 +0100

    inital

--- hetzner-sno-provision-host.sh
+++ hetzner-sno-provision-host.sh
@@ -7,21 +7,28 @@
 set -euo pipefail
 
 IPXE_SCRIPT_URL="${1}"
-curl -f -s -o discovery_ipxe_script.txt "${IPXE_SCRIPT_URL}"
-
+echo wget -O discovery_ipxe_script.txt "${IPXE_SCRIPT_URL}"
+wget -O discovery_ipxe_script.txt "${IPXE_SCRIPT_URL}"
 echo 'This script is meant to be run in the rescue environment to provision the Hetzner node so it can be discovered by assisted installer'
 
 # Set right defaults for kexec-tools and install it
 echo kexec-tools kexec-tools/use_grub_config select false | debconf-set-selections
 echo kexec-tools kexec-tools/load_kexec select true | debconf-set-selections
 
-apt-get install -y kexec-tools
+#apt-get install -y kexec-tools
 
-INITRD_URL="$(awk '/^initrd/{print $NF}' discovery_ipxe_script.txt)"
-KERNEL_URL="$(awk '/^kernel/{print $2}' discovery_ipxe_script.txt)"
-KERNEL_CMDLINE="$(grep '^kernel' discovery_ipxe_script.txt  | cut -d' ' -f 3-)"
+INITRD_URL="$(awk '/^initrd/{print $NF}' discovery_ipxe_script.txt | sed 's/http:/https:/')"
+KERNEL_URL="$(awk '/^kernel/{print $2}' discovery_ipxe_script.txt | sed 's/http:/https:/')"
+KERNEL_CMDLINE="$(grep '^kernel' discovery_ipxe_script.txt  | cut -d' ' -f 3-) | sed 's/http:/https:/')"
 
-curl -f -s -o kernel "${KERNEL_URL}"
-curl -f -s -o initrd "${INITRD_URL}"
+echo "KERNEL_URL[${KERNEL_URL}]"
+echo "INITRD_URL[${INITRD_URL}]"
+echo "KERNEL_CMDLINE[${KERNEL_CMDLINE}]"
+
+echo wget -O kernel $KERNEL_URL
+wget -O kernel $KERNEL_URL
+
+echo wget -O initrd $INITRD_URL
+wget -O initrd $INITRD_URL
 
 kexec kernel --initrd=initrd --append="${KERNEL_CMDLINE}"
